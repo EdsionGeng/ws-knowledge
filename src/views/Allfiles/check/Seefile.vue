605cc324fb173d445090b3d260e4fabfb5ea3651
tangxianju <1597113017@qq.com>
2017-09-14 16:56:49 +0800

    查看文件

--- src/views/Allfiles/check/Seefile.vue
+++ src/views/Allfiles/check/Seefile.vue
@@ -2,15 +2,18 @@
   <div>
     <check></check>
     <Forms></Forms>
+    <Btn></Btn>
   </div>
 </template>
 <script>
 import check from "./check"
 import Forms from "../../Notice/components/Forms"
+import Btn from "../../Allfiles/Btn"
 export default {
   components:{
     check,
-    Forms
+    Forms,
+    Btn
   }
 }
 </script>
