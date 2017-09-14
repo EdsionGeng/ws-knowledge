81cdd47d17b5d8ff3ef6daf0e0f08f28b91a0b20
tangxianju <1597113017@qq.com>
2017-09-14 13:32:28 +0800

    Card

--- src/views/DocManage/components/Imgs.vue
+++ src/views/DocManage/components/Imgs.vue
@@ -1,18 +1,47 @@
 <template>
-<div class='Card'>
-    <Card style="width:320px">
-        <div style="text-align:center">
+<div class='Card clearfix'>
+    <Card style="width:320px"  class='fl' v-for="item in movieList">
+        <div style="text-align:center;">
             <img src="../../../assets/bg.jpg">
-            <h3>标题:</h3>
-            <h3>07/23 10:12</h3>
-            <h3>16511次下载</h3>
+            <h3>标题:{{item.title}}</h3>
+            <h3>{{item.timer}}</h3>
+            <h3>{{item.down}}次下载</h3>
         </div>
     </Card>
 </div>
 </template>
 <script>
     export default {
-        
+        data(){
+          return {
+            movieList:[
+              {
+                url:"../../../assets/bg.jpg",
+                title:"文档1",
+                timer:"07/23 11:12",
+                down:"8901"
+              },
+               {
+                url:"../../../assets/bg.jpg",
+                title:"文档2",
+                timer:"07/23 11:12",
+                down:"8901"
+              },
+               {
+                url:"../../../assets/bg.jpg",
+                title:"文档3",
+                timer:"07/23 11:12",
+                down:"8901"
+              },
+               {
+                url:"../../../assets/bg.jpg",
+                title:"文档4",
+                timer:"07/23 11:12",
+                down:"8901"
+              }
+            ]
+          }
+        }
     }
 </script>
 <style scoped>
@@ -21,5 +50,18 @@ width: 218px;
 }
 .Card h3{
   text-align: left;
+}
+.fl{
+  float: left;
+}
+.clearfix:after {
+	content: "";
+	display: block;
+	visibility: hidden;
+	height: 0;
+	clear: both;
+}
+.clearfix {
+	zoom: 1;
 }
 </style>
