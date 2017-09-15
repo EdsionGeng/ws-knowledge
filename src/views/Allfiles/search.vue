afc114b7de51164102b135d1c2e8abe4e1ea06fe
tangxianju <1597113017@qq.com>
2017-09-15 10:48:18 +0800

    tree

--- src/views/Allfiles/search.vue
+++ src/views/Allfiles/search.vue
@@ -1,49 +1,108 @@
 <template>
-<div class='search'>
-    <Select v-model="model1" style="width:200px">
-        <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-    </Select>
+<div style='margin-top: 50px;'>
+    <AutoComplete
+        v-model="value4"
+        icon="ios-search"
+        placeholder="input here"
+        style="width:300px">
+        <tree></tree>
+        <!-- <div class="demo-auto-complete-item" v-for="item in data4">
+            <div class="demo-auto-complete-group">
+                <span>{{ item.title }}</span>
+                <a href="https://www.google.com/search?q=iView" target="_blank">更多</a>
+            </div>
+            <Option v-for="option in item.children" :value="option.title" :key="option.title">
+                <span class="demo-auto-complete-title">{{ option.title }}</span>
+                <span class="demo-auto-complete-count">{{ option.count }} 人关注</span>
+            </Option>
+        </div> -->
+        <!-- <a href="https://www.google.com/search?q=iView" target="_blank" class="demo-auto-complete-more">查看所有结果</a> -->
+    </AutoComplete>
 </div>
 </template>
 <script>
+import tree from "../Allfiles/tree"
     export default {
         data () {
             return {
-                cityList: [
+                value4: '',
+                data4: [
                     {
-                        value: 'beijing',
-                        label: '北京市'
+                        title: '话题',
+                        children: [
+                            {
+                                title: 'iView',
+                                count: 10000,
+
+                            },
+                            {
+                                title: 'iView UI',
+                                count: 10600,
+
+                            }
+                        ]
                     },
                     {
-                        value: 'shanghai',
-                        label: '上海市'
+                        title: '问题',
+                        children: [
+                            {
+                                title: 'iView UI 有多好',
+                                count: 60100,
+
+                            },
+                            {
+                                title: 'iView 是啥',
+                                count: 30010,
+
+                            }
+                        ]
                     },
                     {
-                        value: 'shenzhen',
-                        label: '深圳市'
-                    },
-                    {
-                        value: 'hangzhou',
-                        label: '杭州市'
-                    },
-                    {
-                        value: 'nanjing',
-                        label: '南京市'
-                    },
-                    {
-                        value: 'chongqing',
-                        label: '重庆市'
+                        title: '文章',
+                        children: [
+                            {
+                                title: 'iView 是一个设计语言',
+                                count: 100000,
+
+                            }
+                        ]
                     }
-                ],
-                model1: ''
+                ]
             }
+        },
+        components:{
+            tree
         }
     }
 </script>
-<style scoped>
-.search{
-  padding: 20px;
-}
+<style>
+    .demo-auto-complete-item{
+        padding: 4px 0;
+        border-bottom: 1px solid #F6F6F6;
+    }
+    .demo-auto-complete-group{
+        font-size: 12px;
+        padding: 4px 6px;
+    }
+    .demo-auto-complete-group span{
+        color: #666;
+        font-weight: bold;
+    }
+    .demo-auto-complete-group a{
+        float: right;
+    }
+    .demo-auto-complete-count{
+        float: right;
+        color: #999;
+    }
+    .demo-auto-complete-more{
+        display: block;
+        margin: 0 auto;
+        padding: 4px;
+        text-align: center;
+        font-size: 12px;
+    }
 </style>
+
 
 
