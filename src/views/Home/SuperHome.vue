b84ebe6df0c6919c81920a64196f4ff74af213cf
李海涛 <15393243613@163.com>
2017-09-18 15:51:48 +0800

    引入echarts图表按需使用

--- src/views/Home/SuperHome.vue
+++ src/views/Home/SuperHome.vue
@@ -1,20 +1,25 @@
 <template>
-<div style='margin-top: 50px;'>
-    <Progress :percent="25" class='fl'></Progress>
-    <Progress :percent="45" status="active"></Progress>
-    <Progress :percent="65" status="wrong"></Progress>
-    <Progress :percent="100"></Progress>
-    <Progress :percent="25" hide-info></Progress>
+<div>
+     <Card shadow>
+        <p slot="title">文件数据统计</p>
+        
+    </Card>
 </div>
 </template>
 <script>
+    import echarts from 'echarts/lib/echarts';
     export default {
-        
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+            testechart(){
+            }
+        }
     }
 </script>
 <style scoped>
-.fl{
-  float: left;
-}
 </style>
 
