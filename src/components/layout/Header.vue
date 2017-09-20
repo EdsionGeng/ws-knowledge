77dba4d78786d43945103c4d2fa646d2a1bb9740
李海涛 <15393243613@163.com>
2017-09-20 10:14:43 +0800

    登陆逻辑修改

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -4,55 +4,41 @@
         <h1>温商贷知识库</h1>
     </div>
     <div class="header-right">
-        <Dropdown>
+        <Dropdown @on-click="onChange">
             <a href="javascript:void(0)">
                 {{userInfo}}
                 <Icon type="arrow-down-b"></Icon>
             </a>
             <Dropdown-menu slot="list" >
-                <Dropdown-item>
-                   <p @click="modal1 = true">修改密码</p>
-                    <Modal
-                        v-model="modal1"
-                        title="修改密码"
-                        @on-ok="ok"
-                        @on-cancel="cancel">
-                        <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
-                            <FormItem prop="user">
-                                <Input type="text" v-model="formInline.user" placeholder="Username">
-                                    <Icon type="ios-person-outline" slot="prepend"></Icon>
-                                </Input>
-                            </FormItem>
-                            <FormItem prop="password">
-                                <Input type="password" v-model="formInline.password" placeholder="Password">
-                                    <Icon type="ios-locked-outline" slot="prepend"></Icon>
-                                </Input>
-                            </FormItem>
-                            <FormItem>
-                                <Button type="primary" @click="handleSubmit('formInline')">登录</Button>
-                            </FormItem>
-                        </Form>
-                    </Modal>
+                <Dropdown-item name="修改密码">
+                   <p>修改密码</p>
                 </Dropdown-item>
-                <Dropdown-item name="Logout">
-                     <p @click="modal2 = true">退出登陆</p>
-                    <Modal v-model="modal2" width="360">
-                        <p slot="header" style="color:#f60;text-align:center">
-                            <Icon type="information-circled"></Icon>
-                            <span>退出确认</span>
-                        </p>
-                        <div style="text-align:center">
-                           
-                            <p>是否继续退出登陆？</p>
-                        </div>
-                        <div slot="footer">
-                            <Button type="error" size="large" long :loading="modal_loading" @click="del">退出</Button>
-                        </div>
-                    </Modal>
+                <Dropdown-item name="退出登录"> 
+                     <p>退出登陆</p>
                 </Dropdown-item>
             </Dropdown-menu>
         </Dropdown>
     </div>
+    <Modal v-model="isShow">
+        <p slot="header">
+            <span>修改密码</span>
+        </p>
+        <Form ref="updatepwd" :model="updatepwd" :rules="updatepwdrules" label-postion="left" :label-width="80">
+            <FormItem label="原密码" prop="oldpwd">
+                <Input :model="updatepwd.oldpwd" placeholder="请输入原密码" :autofocus="true"></Input>
+            </FormItem>
+            <FormItem label="新密码" prop="newpwd">
+                <Input :model="updatepwd.newpwd" placeholder="请输入新密码"></Input>
+            </FormItem>
+            <FormItem>
+                <Button type="primary" @click="handleSubmit('updatepwd')">确认</Button>
+                <!-- 加一个表单重置 -->
+            </FormItem>
+        </Form>
+        <div slot="footer">
+            <p>请牢记自己的新密码哦！</p>
+        </div>
+    </Modal>
 </div>
 </template>
 <script>
@@ -60,67 +46,48 @@ import {logout} from '../../api/login'
 export default {
     data(){
         return{
-            value: '',
-            value2: '',
-            modal1: false,
-            modal2: false,
-            modal_loading: false,
             userInfo:this.getUserName(),
-             formInline: {
-                    user: '',
-                    password: ''
-                },
-                ruleInline: {
-                    user: [
-                        { required: true, message: '请填写用户名', trigger: 'blur' }
-                    ],
-                    password: [
-                        { required: true, message: '请填写密码', trigger: 'blur' },
-                        { type: 'string', min: 6, message: '密码长度不能小于6位', trigger: 'blur' }
-                    ]
-                }
+            isShow:false,
+            updatepwd:{
+                oldpwd:'',
+                newpwd:''
+            },
+            updatepwdrules:{
+                oldpwd:[
+                    { required:true,message:'请填写原密码',trigger:'blur' },
+                    { type:'string',min:6,message:'密码长度不能小于6位',trigger:'blur'}
+                ],
+                newpwd:[
+                    { required:true,message:'请填写新密码',trigger:'blur' },
+                    { type:'string',min:6,message:'密码长度不能小于6位',trigger:'blur'}
+                ]
+            }
         }
     },
     methods:{
-         ok () {
-                this.$Message.info('点击了确定');
-            },
-            cancel () {
-                this.$Message.info('点击了取消');
-            },
         getUserName(){
             return sessionStorage.getItem('UserName');
         },
-        del () {
-           logout().then(res=>{
-                    if(res.data.code != 0){
-                         this.modal_loading = true;
-                        setTimeout(() => {
-                            this.modal_loading = false;
-                            this.modal2 = false;
-                            this.$Message.success('退出成功');
-                            this.$router.push('/Login');
-                        }, 2000);
-                    }else{
-                         this.modal_loading = false;
-                         this.modal2 = false;
-                         this.$Message.success('退出失败');
-                    }
-                   
-                    
+        onChange(name){
+            if(name==="修改密码"){
+                this.isShow=true;
+            }else if(name==="安全退出"){
 
-             
-                })
-               
+            }   
         },
-        handleSubmit(name) {
-                this.$refs[name].validate((valid) => {
-                    if (valid) {
-                        this.$Message.success('提交成功!');
-                    } else {
-                        this.$Message.error('表单验证失败!');
-                    }
-                })
+        handleSubmit(name){
+            this.$refs[name].validate((valid)=>{
+                if(valid){
+                    // 与服务端交互，确认密码是否修改成功
+                    this.$Modal.remove();
+                }else{
+
+                }
+            })
+        },
+        loginOut(){
+            // 安全退出，清空所有的sessionStorge并将路由调转到'/login'
+            
         }
     }
 
