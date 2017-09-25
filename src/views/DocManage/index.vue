519b8399b1a2ae25322d38aaec3c2e243723da92
李海涛 <15393243613@163.com>
2017-09-25 16:11:38 +0800

    fix

--- src/views/DocManage/index.vue
+++ src/views/DocManage/index.vue
@@ -3,7 +3,7 @@
     <h2>文档类型管理</h2>
      <Button type="primary" v-on:click='add()'>add</Button>
     <div>
-     <Tree :data="baseData" v-model='selected' @on-select-change="onslectchange"></Tree>
+     <Tree :data="baseData"  @on-select-change="onslectchange"></Tree>
     </div>
   </div>
 </template>
@@ -11,62 +11,23 @@
     export default {
         data () {
             return {
-               selected: true,
+                // 定义基础数据
                 baseData: [{
-                    id:1,
-                    expand: true,
-                    title: 'parent 1',
-                    children: [{
-                        id:1,
-                        title: 'parent 1-0',
-                        expand: true,
-                        disabled: false,
-                        children: [{
-                            id:0,
-                            title: 'leaf',
-                        }, {
-                           id:0,
-                            title: 'leaf',
-                        }]
-                    }, {
-                        id:1,
-                        title: 'parent 1-1',
-                        expand: true,
-                        checked: true,
-                        children: [{
-                            id:0,
-                            title: '123',
-                        }]
-                    },
-                   
-                    ]
+                    expand:true,
+                    pId:1,
+                    title:'规则制度',
+                    children:[{
+                        expand:true,
+                        typeId:1,
+                        title:'管理制度'
+                    }]
                 }]
             }
         },
         methods:{
-          add(){
-          
-            
-              this.baseData[0].children.push(
-              {
-                        title: 'parent 1-1',
-                        expand: true,
-                        checked: true,
-                        children: [{
-                            title: '123',
-                      }]
-            }
-            )
-            
-          },
-         
           onslectchange(arr){
-            console.log(arr[0].id);
-            if(arr[0].id === "1"){
-              this.add();
-            }else{
-              
-            }
+              // 获取当前选择的数据
+              console.log(arr);
           }
         }
     }
