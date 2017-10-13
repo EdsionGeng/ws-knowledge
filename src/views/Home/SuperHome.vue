9fa0ae8486f4118b4315db45cf4d29d2e37747a3
李海涛 <15393243613@163.com>
2017-10-13 21:07:21 +0800

    更新

--- src/views/Home/SuperHome.vue
+++ src/views/Home/SuperHome.vue
@@ -10,6 +10,9 @@
         </RadioGroup>
         <div :id="id" :style="{width:width,height:height}">
         </div>
+        <Select v-model="model1" style="width:200px">
+        <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+    </Select>
     </Card>
 </div>
 </template>
@@ -40,7 +43,34 @@
         data(){
             return{
                 btnvalue:'',
-                myChart:null
+                myChart:null,
+                cityList: [
+                    {
+                        value: 'beijing',
+                        label: '北京市'
+                    },
+                    {
+                        value: 'shanghai',
+                        label: '上海市'
+                    },
+                    {
+                        value: 'shenzhen',
+                        label: '深圳市'
+                    },
+                    {
+                        value: 'hangzhou',
+                        label: '杭州市'
+                    },
+                    {
+                        value: 'nanjing',
+                        label: '南京市'
+                    },
+                    {
+                        value: 'chongqing',
+                        label: '重庆市'
+                    }
+                ],
+                model1: ''
             }
         },
         mounted(){
