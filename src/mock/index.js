fe39a9cb1c12250ea8bd65c933d00f47c49b1b3d
李海涛 <15393243613@163.com>
2017-12-01 09:14:22 +0800

    删除之前初始化代码

--- src/mock/index.js
+++
@@ -1,6 +0,0 @@
-import Mock from 'mockjs'
-import Login from './login/login.js'
-
-Mock.mock('/Login','post',Login.login);
-
-export default Mock;
--- src/utils/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
- 
-export default {
-    install:function(Vue,name='$http'){
-        Object.defineProperty(Vue.prototype,name,{value:axios});
-    }
-}
