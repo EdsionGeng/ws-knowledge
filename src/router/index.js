56f7b9ff956b92ae2277c0c009843fd34de2bf9c
zw <18755489161@163.com>
2017-12-02 17:04:27 +0800

    增加 index.js  slider的路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,19 @@ import Hello from '@/components/Hello'
 import Home from '@/components/common/Home'
 import DashBoard from '@/components/page/DashBoard'
 import CRUD from '@/components/page/demo1/CRUD'
+import hisUpload from '@/components/page/histroyUpload/histroyUpload'
+import mespush from '@/components/page/mespush/mespush'
+import documentlistManger from '@/components/page/documentlistManger/documentlistManger'
+import documentManger from '@/components/page/documentManger/documentManger'
 import Login from '@/views/login/index'
 
 Vue.use(Router);
 
 
-const constantRouteMap = [
-  { path: '/Login', component: Login },
+const constantRouteMap = [{
+    path: '/Login',
+    component: Login
+  },
   {
     path: '/',
     component: Home,
@@ -21,6 +27,18 @@ const constantRouteMap = [
     }, {
       path: 'CRUD',
       component: CRUD
+    }, {
+      path: 'hisUpload',
+      component: hisUpload
+    }, {
+      path: 'mespush',
+      component: mespush
+    }, {
+      path: 'docoumentManger',
+      component: documentManger
+    }, {
+      path: 'documentlistManger',
+      component: documentlistManger
     }]
   }, {
     path: '*',
