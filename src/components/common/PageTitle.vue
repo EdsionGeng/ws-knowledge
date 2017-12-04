5226bd7a2a12a9e3376d4f69160338487fa0233e
zw <18755489161@163.com>
2017-12-04 16:16:09 +0800

    修改 ： pageTitle.vue+ 修改了组件设计

--- src/components/common/PageTitle.vue
+++ src/components/common/PageTitle.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="pagetitle">
-        <Breadcrumb>
-        <Breadcrumb-item>首页</Breadcrumb-item>
-        <Breadcrumb-item>{{BreadTitle}}</Breadcrumb-item>
+    <Breadcrumb>
+        <Breadcrumb-item v-for='title in BreadTitle' :key='title'>{{title}}</Breadcrumb-item>
     </Breadcrumb>
     </div>
 </template>
@@ -11,8 +10,27 @@ export default {
     props:['BreadTitle']
 }
 </script>
+<style>
+.ivu-breadcrumb-item-separator{
+     color:#009688
+}
+</style>
+
 <style scoped>
 .pagetitle{
     clear: both;
+    border-bottom:1px solid #009688;
 }
+.ivu-breadcrumb{
+    height: 46px;
+    line-height: 46px;
+    font-size: 14px;
+    color:#009688;
+    padding-left:20px;
+}
+.ivu-breadcrumb>span:last-child{
+    font-weight: normal;
+    color:#009688
+}
+
 </style>
