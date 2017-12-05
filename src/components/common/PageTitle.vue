6539f8779bbae6b2cd7f7b65e08ca962a288ed52
zw <18755489161@163.com>
2017-12-05 13:58:25 +0800

    删除：pageTitle.vue + 另加的css格式

--- src/components/common/PageTitle.vue
+++ src/components/common/PageTitle.vue
@@ -1,36 +1,25 @@
 <template>
     <div class="pagetitle">
     <Breadcrumb>
-        <Breadcrumb-item v-for='title in BreadTitle' :key='title'>{{title}}</Breadcrumb-item>
+        <Breadcrumb-item v-for='(title,index) in BreadData' :key='index'>{{title.name}}</Breadcrumb-item>
     </Breadcrumb>
     </div>
 </template>
 <script>
 export default {
-    props:['BreadTitle']
+    props:['BreadData']
 }
 </script>
-<style>
-.ivu-breadcrumb-item-separator{
-     color:#009688
-}
-</style>
-
 <style scoped>
 .pagetitle{
     clear: both;
-    border-bottom:1px solid #009688;
 }
 .ivu-breadcrumb{
     height: 46px;
     line-height: 46px;
     font-size: 14px;
-    color:#009688;
     padding-left:20px;
 }
-.ivu-breadcrumb>span:last-child{
-    font-weight: normal;
-    color:#009688
-}
+
 
 </style>
