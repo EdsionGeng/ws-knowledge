dd53520a57e0b25f93b410d6bc18db6e26230a16
zw <18755489161@163.com>
2017-12-05 09:11:45 +0800

    修改：index.vue + 所有文件的页面

--- src/views/allfiles/index.vue
+++ src/views/allfiles/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
+      <div>
+        <PageTitle :BreadTitle='BreadTitle'></PageTitle>
+      </div>
       这是所有文件页面
   </div>
 </template>
 <script>
+import PageTitle from '@/components/common/PageTitle'
 export default {
   data(){
-      return {};
+      return {
+        BreadTitle:['文件管理','全部文件']
+      };
+  },
+  components:{
+    PageTitle
   }
 
 }
