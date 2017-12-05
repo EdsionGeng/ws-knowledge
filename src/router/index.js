6952fab1032eb038843837c346a9a04c3e14bfcf
zw <18755489161@163.com>
2017-12-05 13:46:02 +0800

    修改：index.js+ 路由配合面包栏

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,8 @@ import Hello from '@/components/Hello'
 import Home from '@/components/common/Home'
 import DashBoard from '@/components/page/DashBoard'
 import CRUD from '@/components/page/demo1/CRUD'
-import hisupload from '@/views/historyupload/index' //历史上传
-import allfiles from '@/views/allfiles/index' //全部文件
+import hisupload from '@/views/historyupload/index'                       //历史上传
+import allfiles from '@/views/allfiles/index'                             //全部文件
 import docdirmanagement from '@/views/docdirmanagement/index'//文档目录管理
 import docmanagement from '@/views/docmanagement/index'//文档管理
 import keySearchPage from '@/views/keySearchPage/index'//关键词搜索结果页面
@@ -32,7 +32,10 @@ const constantRouteMap = [{
     children: [
       {
         path: 'admin',
-        component: AdminHome
+        component: AdminHome,
+        meta:{
+          pname: '首页'
+        }
       }
       ,{
       path: 'DashBoard',
@@ -42,37 +45,84 @@ const constantRouteMap = [{
       component: CRUD
     }, {
       path: 'hisupload',
-      component: hisupload
+      component: hisupload,
+      meta:{
+        pname: '文件管理',
+        cname: '历史上传'
+      }
     }, {
       path: 'mymessage',
-      component: mymessage
+      component: mymessage,
+      meta:{
+        pname: '个人中心',
+        cname: '我的消息'
+      }
     }, {
       path: 'docmanagement',
-      component: docmanagement
+      component: docmanagement,
+      meta:{
+        pname: '管理中心',
+        cname: '文档管理'
+      }
     }, {
       path: 'docdirmanagement',
-      component: docdirmanagement
+      component: docdirmanagement,
+      meta:{
+        pname: '管理中心',
+        cname: '文档目录管理'
+      }
     },{
       path: 'keySearchPage',
-      component: keySearchPage
+      component: keySearchPage,
+      meta:{
+        pname: '搜索文件'
+      }
     },{
       path: 'allfiles',
-      component: allfiles
+      component: allfiles,
+      meta:{
+        pname: '文件管理',
+        cname: '全部文件'
+      }
     },{
       path: 'newfile',
-      component: newfile
+      component: newfile,
+      meta:{
+        pname: '新建文件'
+        
+      }
     },{
       path: 'check',
-      component: check
+      component: check,
+      meta:{
+        pname: '文件管理',
+        cname: '全部文件',
+        tname: '查看文件'
+      }
     },{
       path: 'log',
-      component: log
+      component: log,
+      meta:{
+        pname: '文件管理',
+        cname: '全部文件',
+        tname: '查看文件',
+        lname: '查看日志'
+      }
     },{
       path: 'change',
-      component: change
+      component: change,
+      meta:{
+        pname: '文件管理',
+        cname: '全部文件',
+        tname: '修改文件'
+      }
     },{
       path: 'messagemanage',
-      component: messagemanage
+      component: messagemanage,
+      meta:{
+        pname: '管理中心',
+        cname: '消息管理'
+      }
     }]
   }, {
     path: '*',
