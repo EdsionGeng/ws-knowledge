f6703a738b35413ce858e3fa0b97e8e4586aa50f
zw <18755489161@163.com>
2017-12-06 16:31:33 +0800

    修改：index.vue+的全部文件的页面设计

--- src/views/allfiles/index.vue
+++ src/views/allfiles/index.vue
@@ -1,25 +1,42 @@
 <template>
-  <div>
-      <div>
-        <PageTitle :BreadTitle='BreadTitle'></PageTitle>
-      </div>
-      这是所有文件页面
-  </div>
+<div class="page">
+   部门： <Select v-model="model7" style="width:200px">
+        <Tree :data="deptlist"></Tree> 
+    </Select>
+</div>  
 </template>
 <script>
-import PageTitle from '@/components/common/PageTitle'
-export default {
-  data(){
-      return {
-        BreadTitle:['文件管理','全部文件']
-      };
-  },
-  components:{
-    PageTitle
-  }
+ import { getDepTree } from "../../api/all_interface";
 
-}
+    export default {
+        data () {
+            return {
+              data:null,
+              deptlist:[],
+              model7:''
+            }
+        },
+        mounted(){this.showDepTree()},
+        methods:{
+          showDepTree(){
+             getDepTree(this.data)
+              .then(res => {
+                console.log(res)
+                const data = res.data;
+      //          console.log(res.data);
+                if (res.status == 200) {
+                   //this.deptlist=this.push(name)
+                  this.deptlist=data[0].children
+                  console.log(this.deptlist)
+                }
+              })
+              .catch(err => {});  
+          }
+        }
+    }
 </script>
 <style scoped>
-
+  .page{
+    padding:10px;
+  }
 </style>
