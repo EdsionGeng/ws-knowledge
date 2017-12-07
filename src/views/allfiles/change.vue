edeb9ad5e6e6544281c87e6a2ba48b6fd7cab718
zw <18755489161@163.com>
2017-12-07 14:51:34 +0800

    增加：change.vue：修改文件的页面

---
+++ src/views/allfiles/change.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      这是所有文件、修改文件页面
+  </div>
+</template>
+<script>
+export default {
+      data() {
+        return {
+          BreadTitle: ["文件管理", "全部文件", "修改文件"]
+        };
+      }
+  }
+</script>
+<style scoped>
+
+</style>
+// 增加： change.vue+这是所有文件、修改文件页面
