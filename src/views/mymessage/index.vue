2de8e703538d429a80298de2361c956c8a3d086d
zw <18755489161@163.com>
2017-12-08 19:31:24 +0800

    修改：index.vue+增加了点击公告显示详情

--- src/views/mymessage/index.vue
+++ src/views/mymessage/index.vue
@@ -6,32 +6,35 @@
     <div style="margin:10px">
       <Row class="table-top">
         <Col>
-        <Table :columns="MyMsgColumns" :data="MyMessageList" v-model="showStripe" ></Table>
+        <Table :columns="MyMsgColumns" :data="MyMessageList" showStripe  @on-row-click="onRowClick"></Table>
         </Col>
       </Row>
      <!--<Page :total="page.total" :current="page.current" :page-size="page.pageSize" :show-total="true" @on-change="onPageChange" class="table-page"></Page>-->
     </div>
-    <!--<Modal-->
-      <!--v-model="modal1"-->
-      <!--title="Common Modal dialog box title"-->
-      <!--@on-ok="ok"-->
-      <!--@on-cancel="cancel">-->
-
-    <!--</Modal>-->
+    <Modal
+          :title="mymessageDetail.MyMessageListTitle"
+          v-model="modal1"
+          :mask-closable="false"
+          >
+          <p style='text-align:center'>发布时间：{{ mymessageDetail.MyMessageupdate}}&nbsp; &nbsp;发布人：{{ mymessageDetail.AddUser}}&nbsp;&nbsp;已读人：{{mymessageDetail.readed }}&nbsp;&nbsp;未读人：{{mymessageDetail.noreaded}}</p>
+          <p class="modal-content" style='color:#333;margin-top:10px;'>{{mymessageDetail.MyMessageMsg}}</p>
+      </Modal>
   </div>
 
 </template>
 <script>
   import { showUserAd } from "../../api/all_interface";
+  import { showAdPcs } from "../../api/all_interface";
 export default {
   data(){
       return {
+        modal1:false,
         MyMsgColumns: [
           {
             title: "标题",
             key: "at",
 
-          },
+          },  
           {
             title: "时间",
             key: "re",
@@ -39,6 +42,14 @@ export default {
             align: 'center',
 
           }],
+           mymessageDetail:{
+                MyMessageListTitle:'ewrwqre',
+                MyMessageupdate:'',
+                AddUser:'',
+                MyMessageMsg:'hello world hello you are welcome',
+                readed:0,
+                noreaded:0
+              },   
         showStripe:true,
         MyMessageList: [],
         page: {},
@@ -76,7 +87,30 @@ this.initUserAd();
           }
         })
         .catch(err => {});
-    }
+    }, 
+      onRowClick(row,index){           
+             this.params={
+               commonId:row.commonId
+             };
+             console.log(this.params.commonId)
+              showAdPcs(this.params).then(
+              res => {
+                const data = res.data;
+                  console.log(res.data);
+                if (data.code == 0) {                
+                  this.mymessageDetail.MyMessageListTitle=row.at
+                  this.mymessageDetail.MyMessageupdate=row.re
+                  this.mymessageDetail.AddUser=row.AddUser
+                  this.mymessageDetail.MyMessageMsg=row.ad
+                  this.mymessageDetail.readed=data.data.isRead
+                  this.mymessageDetail.noreaded=data.data.noRead           
+                  }                
+                                
+              }
+            )  
+            .catch(err => {});  
+           this.modal1=true;
+          },
   }
 }
 </script>
