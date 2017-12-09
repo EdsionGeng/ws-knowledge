dffd91ba514efd67807d09faab6fc9d9f484c013
zw <18755489161@163.com>
2017-12-09 17:03:29 +0800

    完成：docTree.vue+自定了文档目录的单文件

---
+++ src/components/common/docTree.vue
@@ -0,0 +1,64 @@
+<template>
+<div style='display:inline-block;position:relative'>
+    <Input v-model="myvalue.value" icon="arrow-down-b" placeholder="类型" @on-click='dropmeun(!showMenu)' style="width: 200px">   
+    </Input>
+    <div v-show='showMenu' class="showmenubox">
+         <Tree :data="deptlist"  multiple   :render="renderContent"></Tree>   
+    </div> 
+</div>
+</template>
+<script>
+import { getDepTree } from "../../api/all_interface";
+export default {
+    props:["deptlist","myvalue"],
+  data() {
+    return {
+      showMenu: false
+    };
+  },
+  methods: {
+    renderContent(h, { root, node, data }) {
+      return h(
+        "span",
+        {
+          style: {
+            cursor: "pointer",
+            marginLeft: "5px"
+          },
+          on: {
+            click: () => {
+              this.myvalue.value = data.fileKindName;
+              this.showMenu = false;
+            }
+          }
+        },
+        data.fileKindName
+      );
+    },
+    dropmeun(isshow) {
+      this.showMenu = isshow;
+    }
+  }
+};
+</script>
+<style scoped>
+.showmenubox {
+  border: 1px solid #eee;
+      margin: 5px 0;
+    padding: 5px 0;
+  min-width: 200px;
+  border-radius: 4px;
+  position: absolute;
+  top: 32px;
+  left: 0;
+  z-index: 9999;
+  background: #fff;
+  transform-origin: center top 0px;
+  box-shadow: 0 1px 6px rgba(0,0,0,.2);
+  max-height:200px;
+  overflow: auto;
+}
+.showmenubox.hide {
+  height: 0;
+}
+</style>
