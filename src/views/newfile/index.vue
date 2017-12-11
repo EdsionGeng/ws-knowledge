9698e52bf34af6d0a558b56e1f80f5d240998f72
zw <18755489161@163.com>
2017-12-11 16:02:13 +0800

    修改：index.vue+修改了新建文件中输入验证

--- src/views/newfile/index.vue
+++ src/views/newfile/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <div class="body-area">
-      <Form>
-        <FormItem label=" 文件标题：">
-          <Input v-model="addFileParams.title" placeholder="" style="width: 200px"></Input>
+      <Form :rules="ruleValidate" label-position="right" :label-width="100" :model='uploadForm' ref='formValidate'>
+        <FormItem label=" 文件标题：" prop="title">
+          <Input v-model="uploadForm.title" placeholder="请输入标题" style="width: 200px"></Input>
         </FormItem>
-        <FormItem label="文档类型：">
-          <docTree  :myvalue='docTypeKey'></docTree>
+        <FormItem label="文档类型：" prop="value">
+          <docTree  :myvalue='uploadForm'></docTree>
         </FormItem>
-        <FormItem label="封面:">
-          <div class="back">
+        <FormItem label="封面：">
+          <div >
             <Upload
               :before-upload="handleUpload"
               action="//jsonplaceholder.typicode.com/posts/">
@@ -22,24 +22,38 @@
           </div>
         </FormItem>
 
-        <FormItem label="内容:">
+        <FormItem label="内容：" prop='content'>
           <div class="hello">
             <div id="editor" type="text/plain" style="width:900px;height:400px;"></div>
-            <Button @click="submits" type="primary">保存</Button>
-            <Button @click="xieru" type="primary">写入</Button>
+           <div style='margin-top:10px;'>
+             <Button @click="submits" type="primary">保存</Button>
+             <Button @click="xieru" type="primary">写入</Button>
+           </div>
+            
           </div>
         </FormItem>
-        <FormItem label="上传附件:">
+        <FormItem label="上传附件：" >
 
           <div class="uploadBtn">
             <Button type="primary" size="small" @click="uploadDoc=true">上传附件</Button>
           </div>
         </FormItem>
-        <FormItem label="权限设置:">
-          <Tabs value="name1">
-            <TabPane label="可查阅人员" name="name1"> <div><Tree :data="depTree" show-checkbox multiple :render="renderContentDep" ></Tree></div></TabPane>
-            <TabPane label="可编辑人员" name="name2"><div> <Tree :data="depTree" show-checkbox multiple :render="renderContentDep" ></Tree></div></TabPane>
-            <TabPane label="可删除人员" name="name3"><div> <Tree :data="depTree" show-checkbox multiple :render="renderContentDep" ></Tree></div></TabPane>
+        <FormItem label="权限设置：" prop='power' >
+          <Tabs value="name1" type='card' class='newfileTab'  style="width:900px;">
+            <TabPane label="可查阅人员" name="name1"> 
+              <div>
+              <Tree :data="depTree" show-checkbox multiple :render="renderContentDep" @on-check-change='chooseCheckPeople'></Tree>
+              </div>
+            </TabPane>
+            <TabPane label="可编辑人员" name="name2"><div> 
+              <Tree :data="editdepTree" show-checkbox multiple :render="renderContentDep" @on-check-change='chooseEditPeople'>></Tree>
+              </div>
+            </TabPane>
+            <TabPane label="可删除人员" name="name3">
+              <div> 
+                <Tree :data="deldepTree" show-checkbox multiple :render="renderContentDep"@on-check-change='chooseDelPeople'> ></Tree>
+              </div>
+            </TabPane>
           </Tabs>
 
         </FormItem>
@@ -54,6 +68,9 @@
         <!--<div v-if="file !== null">Upload file: {{ file.name }} <Button type="text" @click="upload" :loading="loadingStatus">{{ loadingStatus ? '内容' : ' 确定上传？' }}</Button></div>-->
         <!--</div>-->
         <!--</FormItem>-->
+        <FormItem label="">
+          <Button type='primary' size='large' @click="handleSubmit('formInline')">确定上传</Button>
+        </FormItem>
       </Form>
 
     </div>
@@ -90,138 +107,164 @@
   </div>
 </template>
 <script>
-  import '../../../static/utf8-jsp/ueditor.config'
-  import '../../../static/utf8-jsp/ueditor.all';
-  import '../../../static/utf8-jsp/lang/zh-cn/zh-cn';
-  import docTree from '@/components/common/docTree'
-  import {getDocTree} from "../../api/all_interface";
-  import {getDepTree} from "../../api/all_interface";
-  import {queryUserByGroup} from "../../api/all_interface";
-  export default {
-    data() {
-      return {
-         docTypeKey:{
-          value:''
-        },
-        addFileParams: {
-          title: "",
-          describle: "",
-
-        },
-        showMenu: false,
-        file: null,
-        filedoc: null,
-        loadingStatus: false,
-        fileloadingStatus: false,
-      
-        depTree: [],
-        depTreeParams: {
-          id: "",
-          deptno: "",
-          no: "",
-          pid: "",
-          type: "",
-          checked: "",
-          name: "",
-          department: ""
-        },
-        ue: '',
-        uedata: [],
-        xierudata: [],
-
-
-        uploadDoc: false,
-      };
-    },
-    components:{
-      docTree
-    },
-    mounted() {
-      this.showDepTree;
-      this.ue = UE.getEditor('editor', {
-        BaseUrl: '',
-        UEDITOR_HOME_URL: 'static/utf8-jsp/',
-// toolbars:[]
-      });
-    },
-    methods: {
-
-      renderContentDep(h, {root, node, data}) {
-        return h('span', data.name)
+import "../../../static/utf8-jsp/ueditor.config";
+import "../../../static/utf8-jsp/ueditor.all";
+import "../../../static/utf8-jsp/lang/zh-cn/zh-cn";
+import docTree from "@/components/common/docTree";
+import { getDocTree } from "../../api/all_interface";
+import { getDepTree } from "../../api/all_interface";
+import { queryUserByGroup } from "../../api/all_interface";
+export default {
+  data() {
+    return {
+      ruleValidate: {
+        title: [
+          { required: true,message: '请填写文件标题', trigger: 'blur'},
+          { type: "string", max: 20, message: "标题限制在20字以内"}
+        ],
+        value : [{ required: true, message: '请选择文件类型', trigger: 'blur'}],
+        content: [{ required: true }],
+        power: [{ required: true }],
       },
-      /**
+      uploadForm:{
+        title:'',
+        value:'',
+        content:'',
+        power:''
+      },
+      addFileParams: {
+        title: "",
+        describle: ""
+      },
+      showMenu: false,
+      file: null,
+      filedoc: null,
+      loadingStatus: false,
+      fileloadingStatus: false,
+      // 编辑是根据查看人员进行筛选的
+      depTree: [],
+      editdepTree:[],
+      deldepTree:[],
+
+      depTreeParams: {
+        id: "",
+        deptno: "",
+        no: "",
+        pid: "",
+        type: "",
+        checked: "",
+        name: "",
+        department: ""
+      },
+      ue: "",
+      uedata: [],
+      xierudata: [],
+      uploadDoc: false
+    };
+  },
+  components: {
+    docTree
+  },
+  mounted() {
+    this.showDepTree();
+    this.ue = UE.getEditor("editor", {
+      BaseUrl: "",
+      UEDITOR_HOME_URL: "static/utf8-jsp/"
+      // toolbars:[]
+    });
+  },
+  methods: {
+    chooseCheckPeople(val){
+      console.log(val)
+      let checkval =val;
+      let editval =val;
+      this.uploadForm.power=val
+      this.editdepTree=checkval;
+      this.deldepTree=editval;
+      },
+    chooseDelPeople(val){console.log(val)},
+    chooseEditPeople(val){console.log(val)},
+    renderContentDep(h, { root, node, data }) {
+      return h("span", data.name);
+    },
+    /**
        * 获取文档树形结构
        */
-    
-      showDepTree() {
-        let _self = this
-        getDepTree(_self.depTreeParams).then(res => {
-          _self.depTree = res.data;
-        })
-      },
-      dropmenu(isshow) {
-        this.showMenu = isshow;
-        console.log(1212);
-      },
-      handleUpload(file) {
-        this.file = file;
-        return false;
-      },
-      handleSuccess(res, file) {
-        file.url = 'https://o5wwk8baw.qnssl.com/7eb99afb9d5f317c912f08b5212fd69a/avatar';
-        file.name = '7eb99afb9d5f317c912f08b5212fd69a';
-      },
-      upload() {
-        this.loadingStatus = true;
-        setTimeout(() => {
-          this.file = null;
-          this.loadingStatus = false;
-          this.$Message.success('上传成功')
-        }, 1500);
-      },
-      docupload() {
-        this.fileloadingStatus = true;
-        setTimeout(() => {
-          this.filedoc = null;
-          this.fileloadingStatus = false;
-          this.$Message.success('上传成功')
-        }, 1500);
-      },
-      handleMaxSize(file) {
-        this.$Notice.warning({
-          title: '友情提醒',
-          desc: '文件  ' + file.name + '过大,已超过10M！'
-        });
-      },
-      submits() {
-        this.uedata.push(UE.getEditor('editor').getContent());
-        console.log(this.uedata.join("\n"));
-      },
-      xieru() {
-        UE.getEditor('editor').setContent('欢迎使用ueditor');
-      }
+
+    showDepTree() {
+      let _self = this;
+      getDepTree(_self.depTreeParams).then(res => {
+        _self.depTree = res.data;
+      });
+    },
+    handleUpload(file) {
+      this.file = file;
+      return false;
+    },
+    handleSuccess(res, file) {
+      file.url =
+        "https://o5wwk8baw.qnssl.com/7eb99afb9d5f317c912f08b5212fd69a/avatar";
+      file.name = "7eb99afb9d5f317c912f08b5212fd69a";
+    },
+    upload() {
+      this.loadingStatus = true;
+      setTimeout(() => {
+        this.file = null;
+        this.loadingStatus = false;
+        this.$Message.success("上传成功");
+      }, 1500);
+    },
+    docupload() {
+      this.fileloadingStatus = true;
+      setTimeout(() => {
+        this.filedoc = null;
+        this.fileloadingStatus = false;
+        this.$Message.success("上传成功");
+      }, 1500);
+    },
+    handleMaxSize(file) {
+      this.$Notice.warning({
+        title: "友情提醒",
+        desc: "文件  " + file.name + "过大,已超过10M！"
+      });
+    },
+    submits() {
+      this.uedata.push(UE.getEditor("editor").getContent());
+      console.log(this.uedata)
+      let html=this.uedata.join("\n");
+      console.log(this.uedata.join("\n"));
+      this.uploadForm.content=html;
+    },
+    xieru() {
+      UE.getEditor("editor").setContent("欢迎使用ueditor");
+    },
+    handleSubmit(name) {
+        // 将文件上传中所有输入的信息已保存在uploadForm中
+       console.log(this.uploadForm);
     }
-
-
   }
+};
 </script>
+<style>
+.ivu-tabs-tabpane {
+  border: 1px solid #eee;
+}
+.newfileTab .ivu-tabs-bar {
+  margin-bottom: 0;
+}
+</style>
+
 <style scoped>
-  .body-area {
-    margin-left: 30px;
-  }
+.body-area {
+  padding: 20px;
+}
 
-  .body-area .hello {
-    margin-left: 55px;
+.body-area .hello {
+}
 
-  }
+.body-area .back {
+}
 
-  .body-area .back {
-    margin-left: 55px;
-
-  }
-
-  .body-area .uploadBtn {
-    margin-left: 55px;
-
-  }
+.body-area .uploadBtn {
+}
 </style>
