563a1e98ab7a11c491703e2397f49dba82613928
zw <18755489161@163.com>
2017-12-14 17:00:57 +0800

    修改：index.vue+增加了按类型进行查找功能

--- src/views/allfiles/index.vue
+++ src/views/allfiles/index.vue
@@ -51,15 +51,18 @@
 </template>
 <script>
 import { showUserLookFile } from "../../api/all_interface";
+import { searchResult } from "../../api/all_interface";
 import SelectTree from '@/components/common/selectTree'
 import docTree from '@/components/common/docTree'
 
 export default {
   data() {
     return {
+      active:'showUserLookFile',
       //用对象就可以让子组件修改父组件的内容
       docTypeKey:{
-        value:''
+        value:'',
+        id:0
       },
       depTypeKey:{
         value:''
@@ -122,23 +125,55 @@ export default {
   },
   methods: {
     selAllFile(){
-      console.log(this.docTypeKey.value)
-      console.log(this.depTypeKey.value)
+      let selListParams={
+            current: 1,
+            pageSize: 20,
+            userId: 145,
+            departmentName:this.depTypeKey.value,
+            fileStyleId:this.docTypeKey.id
+       }
+       console.log(selListParams)
+       console.log(
+         this.docTypeKey.id,this.depTypeKey.value
+       )
+      this.initSelList(selListParams)
+      // console.log(this.docTypeKey.id)
+      // console.log(this.depTypeKey.value)
+    },
+    initSelList(selListParams){
+        console.log('执行了搜索页面')
+        searchResult(selListParams)
+        .then(res => {
+          const showUserUpdata = res.data;
+          console.log(showUserUpdata);
+          if (res.data.code == 0) {
+            console.log(res.data)
+            this.page = res.data.rdPage;
+            this.historyUploadMessageList = showUserUpdata.data;
+          }
+        })
+        .catch(err => {});
     },
     onRowClick(row) {
       this.$router.push("/allfiles/check/" + row.id);
     },
-    onPageChange(value) {
+    onPageChange(value) { 
       console.log(value);
       this.page.current = value;
-      this.initList();
+      if(this.active=='showUserLookFile'){
+        this.initList();
+      }else{
+        this.selAllFile();
+      }
     },
     onPageSizeChange(value) {
       this.page.pageSize = value;
     }, 
     initList() {
+        console.log('个人能看到的文件展示列表')
       showUserLookFile(this.listparams)
         .then(res => {
+          console.log('个人能看到的文件展示列表')
           console.log(res)
           const showUserUpdata = res.data;
           console.log(showUserUpdata);
