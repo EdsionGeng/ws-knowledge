9bfc952685c0d30c4dad26a3bb7e0115da2109a6
zw <18755489161@163.com>
2017-12-16 16:58:21 +0800

    修改：search.vue+对搜索框进行参数判断

--- src/components/common/search.vue
+++ src/components/common/search.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="clearfix">
  <div class="search">
-      <Input class='searchInput' size='large' v-model='inputValue'  icon="search" placeholder="搜索" 
+      <Input class='searchInput' size='large' v-model='inputValue'  icon="search" placeholder="搜索"
       style="width: 200px" @on-enter="changekeyRouter('keySearchPage')" @on-click="changekeyRouter('keySearchPage')"></Input>
   </div>
   <div class="creatFile">
        <Button  size='large' icon="plus" type='primary' @click="changeRouter('newfile')">新建文件</Button>
   </div>
 </div>
- 
+
 </template>
 <script>
 export default {
@@ -20,6 +20,9 @@ export default {
     methods:{
         changekeyRouter(name){
           console.log(this.inputValue);
+          if(this.inputValue===''){
+            this.$Message.warning('请输入关键字进行搜索');
+          }
           this.$router.push('/'+name+'/'+this.inputValue);
       },changeRouter(name){
           this.$router.push('/'+name);
@@ -48,13 +51,13 @@ export default {
     .creatFile{
         text-align:center;
         margin-top:20px;
-         
+
     }
         .creatFile button{
             height: 40px;
             width: 200px;
             border-radius:5px;
-            font-size: 16px;  
+            font-size: 16px;
             color:#fff;
         }
 </style>
