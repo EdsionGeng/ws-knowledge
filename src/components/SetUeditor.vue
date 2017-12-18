7d197b2f6a289165afab9e0c68e7c205e1e57fbc
zw <18755489161@163.com>
2017-12-18 19:02:42 +0800

    修改：setUditor.vue+修改了文本编辑器的内容

--- src/components/SetUeditor.vue
+++ src/components/SetUeditor.vue
@@ -3,7 +3,7 @@
     <div type="text/plain" id="ueditor"></div>
     <br>
     <Button @click="submits" type='primary'>保存</Button> 
-    <!-- <Button @click="xieru" type='primary'>写入</Button>  -->
+    <Button @click="xieru" type='primary'>加载已保存的文件内容</Button> 
   </div>
 </template>
 <script>
@@ -11,7 +11,7 @@ import "../../static/utf8-jsp/ueditor.config";
 import "../../static/utf8-jsp/ueditor.all";
 import "../../static/utf8-jsp/lang/zh-cn/zh-cn";
 export default {
-  props:['ueditorContent',''],
+  props:['ueditorContent'],
   name: "set",
   data() {
     return {
@@ -21,6 +21,7 @@ export default {
   },
   mounted() {
       this.ueditor1 = UE.getEditor("ueditor", {
+      serverUrl:'/static/',
       BaseUrl: "",
       initialFrameWidth:900  // null表示宽度自动
       ,initialFrameHeight:400,
@@ -30,6 +31,9 @@ export default {
       //   ['bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc']
       // ]
     });
+   setTimeout(() => {
+     this.xieru();
+   }, 1000); 
   },
   destroyed(){
     this.ueditor1.destroy();
@@ -39,9 +43,9 @@ export default {
       this.ueditorContent.content=UE.getEditor("ueditor").getContent();
       console.log(this.ueditorContent.content)
     },
-    // xieru() {
-    //   UE.getEditor("ueditor1").setContent(this.ueditorText);
-    // }
+    xieru() {
+      UE.getEditor("ueditor").setContent(sessionStorage.getItem('content'));
+    }
   }
 };
 </script>
