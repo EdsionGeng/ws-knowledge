d69c3a5f0e0598d50e1f9d5a7fee421fe974d9ea
zw <18755489161@163.com>
2017-12-21 16:40:55 +0800

    修改：index.vue+修改了全部文件显示页面

--- src/views/historyupload/index.vue
+++ src/views/historyupload/index.vue
@@ -3,25 +3,24 @@
 <div class="page">
         <Row>
         <Col span="24" class="demo-tabs-style1" style="background: #e3e8ee;padding:16px;">
-            <Tabs type="card">
-                <TabPane label="列表" class="clearfix">
+            <Tabs type="card" @on-click='changePic'>
+                <TabPane label="列表" class="clearfix" name='table'>
                   <Row>
                     <Col>
-                      <Table :columns="columns1" 	:data="historyUploadMessageList" @on-row-click="onRowClick"></Table>
+                      <Table class="myTable"  :columns="columns1" stripe	:data="historyUploadMessageList" @on-row-click="onRowClick"></Table>
                     </Col>
                   </Row>
                 </TabPane>
-                <TabPane label="图文">
+                <TabPane label="图文" name='pic'>
                     <Row>
-                      <Col  span="5" offset=1  pull=1 v-for='(list,index) in historyUploadMessageList' :key='index' style='margin-bottom:15px;'>
+                      <Col  span="5" offset=1  pull=1 v-for='(list,index) in historyUploadpicMessageList' :key='index' style='margin-bottom:10px;'>
                         <a @click="onRowClick(list)">
                         <Card style="">
                           <div style="text-align:center">
-                              <img src="./bg.jpg" style='width:60%;height:150px;'>
-                              <h2>{{list.title}}</h2>
-                              <p>上传时间：{{list.addFileTime}}</p>
+                              <img src="./bg.jpg" style='width:50%;height:100px;'>
+                              <h2 class="nowrap" style='color:#000;font-size:19px;'>{{list.title}}</h2>
+                              <p style='color:#ccc'>上传时间：{{list.addFileTime}}</p>
                           </div>
-
                         </Card>
                         </a>
 
@@ -32,16 +31,13 @@
             </Tabs>
 
         </Col>
-
     </Row>
-    <Row style='padding-top:10px,padding-right:5px'>
+    <Row>
         <Col>
-          <Page :total="page.total" :page-size-opts='pageOpts' show-sizer show-elevator :current="page.current" :page-size="page.pageSize" :show-total="true" @on-change="onPageChange" @on-page-size-change='onPageSizeChange' class="table-page"></Page>
+          <Page :total="page.total" placement='top' :page-size-opts='pageOpts' show-sizer show-elevator :current="page.current" :page-size="page.pageSize" :show-total="true" @on-change="onPageChange" @on-page-size-change='onPageSizeChange' class="table-page"></Page>
         </Col>
     </Row>
-
 </div>
-
 </template>
 <script>
 import { showUserUpload } from "../../api/all_interface";
@@ -54,7 +50,8 @@ export default {
       listparams: {
         current: 1,
         pageSize: 20,
-        userId: sessionStorage.getItem('userId')
+        userId: sessionStorage.getItem('userId'),
+        userGroupId:sessionStorage.getItem('userGroupId')
       },
       page: { total: 100,pages:1, current: 1, pageSize: 20 },
       columns1: [
@@ -69,6 +66,7 @@ export default {
           align: "right"
         }
       ],
+      historyUploadpicMessageList:[],
       historyUploadMessageList: [
         {
           title: "hello",
@@ -97,6 +95,14 @@ export default {
     this.initList();
   },
   methods: {
+    changePic(name){
+      console.log(name)
+      if(name==='pic'){
+        this.historyUploadpicMessageList=this.historyUploadMessageList;
+      }else{
+        this.historyUploadpicMessageList=[]
+      }
+    },
     //显示文件详情的方法
     onRowClick(row) {
       this.$router.push('/allfiles/check/'+row.id);
