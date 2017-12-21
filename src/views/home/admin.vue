51d53648d7290856dc704434b5c448bcdd896f5e
zw <18755489161@163.com>
2017-12-21 16:41:21 +0800

    优化了：admin.vue+优化了管理员首页

--- src/views/home/admin.vue
+++ src/views/home/admin.vue
@@ -1,139 +1,128 @@
 <template>
    <div class="pagebox">  
-    <div class="addmin-title">
+    <div class="addmin-title" style="padding-bottom:10px">
       <ButtonGroup>
-        <Button type="ghost" @click='getDayData'>今日</Button>
-        <Button type="ghost" @click="getWeekData">本周</Button>
-        <Button type="ghost" @click="getMonthData">本月</Button>
+        <Button  @click='getDayData("day")' :type="index=='day'?'primary':'ghost'">今日</Button>
+        <Button  @click="getWeekData('week')" :type="index=='week'?'primary':'ghost'">本周</Button>
+        <Button  @click="getMonthData('month')" :type="index=='month'?'primary':'ghost'">本月</Button>
       </ButtonGroup>
     </div>
     <Row class="table-top">
           <Col>
-            <Card dis-hover>
-               <div id='echarts'>
-                </div>
+            <Card dis-hover style="width:600px;">
+                <myPie title="文件编辑信息" :legendData='legend' :seriesData="seriesCash"></myPie>
             </Card>
           </Col>
     </Row>    
   </div>
 </template>
 <script>
+import myPie from "@/components/echarts/pie";
 import { showDayData } from "@/api/all_interface";
 import { showWeekData } from "@/api/all_interface";
 import { showMonthData } from "@/api/all_interface";
-var echarts = require('echarts/lib/echarts');
-        // 引入柱状图
-        require('echarts/lib/chart/bar');
-        // 引入提示框和标题组件
-        require('echarts/lib/component/tooltip');
-        require('echarts/lib/component/title');
-export default {
+  export default {
   data(){
       return{
+       seriesCash:[],
        BreadTitle:['首页'],
-       index:'today',
-       m1date:[2,34,18],
-       series:['上传','修改','删除'],
-       spinShow:false,
+       index:'day',
+       seriescash:[],
+       legend:['上传','修改','删除'],
        params:null
-
       }
   },
-  watch: {
-    series() {
-      this.makeEchart()
-    },
-    m1date() {
-      this.makeEchart()
-    }
+  components:{
+    myPie
   },
   created(){
     this.getDayData()
     },
   mounted(){
-    this.makeEchart()
+    
+    //this.makeEchart()
   },
   methods:{
-      makeEchart(){
-        // 引入 ECharts 主模块
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('echarts'));
-        // 绘制图表
-        myChart.setOption({
-             parallelAxis:{
-               axisLabel:{
-                  fontSize:22,
-                  fontWeight:'bold'
-               }
-             },
-              color: ['#52C6EB'],
-              tooltip : {
-                  trigger: 'axis',
-                  axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                      type : 'line'        // 默认为直线，可选为：'line' | 'shadow'
-                  }
-              },
-              grid: {
-                  left: '2%',
-                  right: '3%',
-                  bottom: '4%',
-                  containLabel: true
-              },
-              xAxis : [
-                  {
-                      type : 'category',
-                      data : this.series,
-                      axisTick: {
-                          alignWithLabel: true
-                      }
-                  }
-              ],
-              yAxis : [
-                  {
-                      type : 'value'
-                  }
-              ],
-              series : [
-                  {
-                      name:'直接访问',
-                      type:'bar',
-                      barWidth: '20%',
-                      data:this.m1date
-                  }
-              ]
-          })
+      ObjecToArray(obj) {
+        let serires = [];
+        for (const key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            const element = obj[key];
+            serires.push(element);
+          }
+        }
+        return serires;
       },
-      getDayData(){
-         this.spinShow=true;
+     
+      getDayData(val){
+          if(val!=undefined){
+             this.index=val;
+          }
         showDayData(this.params).then(res=>{
-            const data = res.data;
-            const bardata=data.data;
-            this.m1date =[],
-            this.m1date[0]=bardata.addpcs;
-            this.m1date[1]=bardata.updatepcs;
-            this.m1date[2]=bardata.deletepcs;
-             console.log(bardata)
+          let data = res.data;
+            if(data.code==0){
+              let bardata=data.data;    
+              this.seriesCash=[
+                  {
+                    value:bardata.addpcs,
+                    name:this.legend[0]
+                  },
+                  {
+                    value:bardata.updatepcs,
+                    name:this.legend[1]
+                  },
+                  {
+                    value:bardata.deletepcs,
+                    name:this.legend[2]
+                   }
+              ]
+            }
         })
-         this.spinShow=true;
       },
-      getWeekData(){
+      getWeekData(val){   
+           this.index=val;
         showWeekData(this.params).then(res=>{
-            let data = res.data;
-            let bardata=data.data;
-            this.m1date =[],
-            this.m1date[0]=bardata.addpcs;
-            this.m1date[1]=bardata.updatepcs;
-            this.m1date[2]=bardata.deletepcs;
+           let data = res.data;
+            if(data.code==0){
+              let bardata=data.data;    
+              this.seriesCash=[
+                  {
+                    value:bardata.addpcs,
+                    name:this.legend[0]
+                  },
+                  {
+                    value:bardata.updatepcs,
+                    name:this.legend[1]
+                  },
+                  {
+                    value:bardata.deletepcs,
+                    name:this.legend[2]
+                   }
+              ]
+            }
         })
       },
-      getMonthData(){
+      getMonthData(val){
+           this.index=val;
         showMonthData(this.params).then(res=>{
             let data = res.data;
-            let bardata=data.data;
-            this.m1date =[],
-            this.m1date[0]=bardata.addpcs;
-            this.m1date[1]=bardata.updatepcs;
-            this.m1date[2]=bardata.deletepcs;
+            if(data.code==0){
+              let bardata=data.data;    
+              this.seriesCash=[
+                  {
+                    value:bardata.addpcs,
+                    name:this.legend[0]
+                  },
+                  {
+                    value:bardata.updatepcs,
+                    name:this.legend[1]
+                  },
+                  {
+                    value:bardata.deletepcs,
+                    name:this.legend[2]
+                   }
+              ]
+            }
         })
       }
     }
