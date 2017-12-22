15734e4fb4e1283584971106e9115d17ad21238a
zw <18755489161@163.com>
2017-12-22 08:29:54 +0800

    修改：index.vue+格式化了全部文件的记录的代码

--- src/views/allfiles/index.vue
+++ src/views/allfiles/index.vue
@@ -1,175 +1,158 @@
 <template>
-<div class="page">
-   <Row align='middle'   class='clearfix'  style='margin-bottom:20px'	>
-        <Col span="1" offset=1  style='height:30px;line-height:30px;padding-left:20px;'> 部门:</Col>
-        <Col span="4">
-            <SelectTree  :myvalue='depTypeKey'></SelectTree>
-        </Col>
-        <Col span="1"  style='height:30px;line-height:30px;padding-left:20px;'> 类型:</Col>
-        <Col span="4">
-          <docTree  :myvalue='docTypeKey'></docTree>
-        </Col>
-          </Col> <Button type='primary' @click="selAllFile">查询</Button></Col>
+  <div class="page">
+    <Row align='middle' class='clearfix' style='margin-bottom:20px'>
+      <Col span="1" offset=1 style='height:30px;line-height:30px;padding-left:20px;'>
+      部门:</Col>
+      <Col span="4">
+      <SelectTree :myvalue='depTypeKey'></SelectTree>
+      </Col>
+      <Col span="1" style='height:30px;line-height:30px;padding-left:20px;'>
+      类型:</Col>
+      <Col span="4">
+      <docTree :myvalue='docTypeKey'></docTree>
+      </Col>
+      </Col>
+      <Button type='primary' @click="selAllFile">查询</Button>
+      </Col>
     </Row>
-      <Row>
-          <Col span="24" class="demo-tabs-style1" style="background: #e3e8ee;padding:16px;">
-              <Tabs type="card" @on-click='changePic'>
-                  <TabPane label="列表" name='table'>
-                    <Row>
-                      <Col>
-                        <Table :columns="columns1" stripe class="myTable"	:data="userLookFileList" @on-row-click="onRowClick"></Table>
-                      </Col>
-                    </Row>
-                  </TabPane>
-                 <TabPane label="图文" name='pic'>
-                    <Row>
-                      <Col  span="5" offset=1  pull=1 v-for='(list,index) in userLookpicFileList' :key='index' style='margin-bottom:8px;'>
-                        <a @click="onRowClick(list)">
-                        <Card style="">
-                         <div style="text-align:center">
-                              <img src="./bg.jpg" style='width:50%;height:90px;'>
-                              <h2 style='color:#000;font-size:19px;' class="nowrap">{{list.title}}</h2>
-                              <p style='color:#ccc'>上传时间：{{list.addFileTime}}</p>
-                              <p style='color:#ccc'>上传人：{{list.username}}</p>
-                          </div>
-                        </Card>
-                        </a>
-                      </Col>
-                  </Row>
-                </TabPane>
-              </Tabs>
-          </Col>
+    <Row>
+      <Col span="24" class="demo-tabs-style1" style="background: #e3e8ee;padding:16px;">
+      <Tabs type="card" @on-click='changePic'>
+        <TabPane label="列表" name='table'>
+          <Row>
+            <Col>
+            <Table :columns="columns1" stripe class="myTable" :data="userLookFileList"
+                   @on-row-click="onRowClick"></Table>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane label="图文" name='pic'>
+          <Row>
+            <Col span="5" offset=1 pull=1 v-for='(list,index) in userLookpicFileList' :key='index'
+                 style='margin-bottom:8px;'>
+            <a @click="onRowClick(list)">
+              <Card style="">
+                <div style="text-align:center">
+                  <img src="./bg.jpg" style='width:50%;height:90px;'>
+                  <h2 style='color:#000;font-size:19px;' class="nowrap">{{list.title}}</h2>
+                  <p style='color:#ccc'>上传时间：{{list.addFileTime}}</p>
+                  <p style='color:#ccc'>上传人：{{list.username}}</p>
+                </div>
+              </Card>
+            </a>
+            </Col>
+          </Row>
+        </TabPane>
+      </Tabs>
+      </Col>
     </Row>
-     <Row style='padding-top:10px,padding-right:5px;' class="bottom-fixed">
-                <Col>
-                  <Page :total="page.total" placement='top' :page-size-opts='pageOpts' show-sizer show-elevator :current="page.current" :page-size="page.pageSize" :show-total="true" @on-change="onPageChange" @on-page-size-change='onPageSizeChange' class="table-page"></Page>
-                </Col>
-      </Row>
+    <Row style='padding-top:10px,padding-right:5px;' class="bottom-fixed">
+      <Col>
+      <Page :total="page.total" placement='top' :page-size-opts='pageOpts' show-sizer
+            :current="page.current" :page-size="page.pageSize" :show-total="true" @on-change="onPageChange"
+            @on-page-size-change='onPageSizeChange' class="table-page"></Page>
+      </Col>
+    </Row>
   </div>
 </template>
 <script>
-import { showUserLookFile } from "../../api/all_interface";
-import SelectTree from "@/components/common/selectTree";
-import docTree from "@/components/common/docTree";
+  import {showUserLookFile} from "../../api/all_interface";
+  import SelectTree from "@/components/common/selectTree";
+  import docTree from "@/components/common/docTree";
 
-export default {
-  data() {
-    return {
-      //用对象就可以让子组件修改父组件的内容
-      docTypeKey: {
-        value: "",
-        id: 0
-      },
-      depTypeKey: {
-        value: ""
-      },
-      pageOpts: [20, 40, 60, 100],
-      listparams: {
-        userId: sessionStorage.getItem("userId"),
-        current: 1,
-        pageSize: 20,
-        userGroupId: 3
-      },
-      page: { total: 100, pages: 1, current: 1, pageSize: 20 },
-      columns1: [
-        {
-          title: "标题",
-          key: "title"
+  export default {
+    data() {
+      return {
+        //用对象就可以让子组件修改父组件的内容
+        docTypeKey: {
+          value: "",
+          id: 0
         },
+        depTypeKey: {
+          value: ""
+        },
+        pageOpts: [20, 40, 60, 100],
+        listparams: {
+          userId: sessionStorage.getItem("userId"),
+          current: 1,
+          pageSize: 20,
+          userGroupId: 3
+        },
+        page: {total: 20, pages: 1, current: 1, pageSize: 20},
+        columns1: [
+          {
+            title: "标题",
+            key: "title"
+          },
 
-        {
-          title: "发布时间",
-          key: "addFileTime",
-          sortable: true,
-          align: "center"
-        },
-        {
-          title: "上传人",
-          key: "username",
-          align: "right"
-        }
-      ],
-      userLookpicFileList:[],
-      userLookFileList: [
-        {
-          title: "hello",
-          addFileTime: "2017-01-01",
-          id: 1
-        },
-        {
-          title: "hello",
-          addFileTime: "2017-01-01",
-          id: 2
-        },
-        {
-          title: "hello",
-          addFileTime: "2017-01-01",
-          id: 3
-        },
-        {
-          title: "hello world",
-          addFileTime: "2017-01-02",
-          id: 4
-        },
-        {
-          title: "hello world",
-          addFileTime: "2017-01-05",
-          id: 5
-        }
-      ]
-    };
-  },
-  components: {
-    SelectTree,
-    docTree
-  },
-  created() {
-    this.initList();
-  },
-  methods: {
-     changePic(name){
-      console.log(name)
-      if(name==='pic'){
-        this.userLookpicFileList=this.userLookFileList;
-      }else{
-        this.userLookpicFileList=[]
-      }
-    },
-    selAllFile() {
-      this.listparams.departmentName = this.depTypeKey.value;
-      this.listparams.fileStyleId = this.docTypeKey.id;
-      this.initList();
-    },
-    onRowClick(row) {
-      this.$router.push("/allfiles/check/" + row.id);
-    },
-    onPageChange(value) {
-      this.listparams.current = value;
-      this.initList();
-    },
-    onPageSizeChange(value) {
-      this.listparams.pageSize = value;
-      this.initList();
-    },
-    initList() {
-      console.log("个人能看到的文件展示列表");
-      console.log(this.listparams);
-      showUserLookFile(this.listparams)
-        .then(res => {
-          const showUserUpdata = res.data;
-          console.log(showUserUpdata);
-          if (res.data.code == 0) {
-            this.page = res.data.rdPage;
-            this.userLookFileList = showUserUpdata.data;
+          {
+            title: "发布时间",
+            key: "addFileTime",
+            sortable: true,
+            align: "center"
+          },
+          {
+            title: "上传人",
+            key: "username",
+            align: "right"
           }
-        })
-        .catch(err => {});
+        ],
+        userLookpicFileList: [],
+        userLookFileList: []
+      };
+    },
+    components: {
+      SelectTree,
+      docTree
+    },
+    created() {
+      this.initList();
+    },
+    methods: {
+      changePic(name) {
+        console.log(name)
+        if (name === 'pic') {
+          this.userLookpicFileList = this.userLookFileList;
+        } else {
+          this.userLookpicFileList = []
+        }
+      },
+      selAllFile() {
+        this.listparams.departmentName = this.depTypeKey.value;
+        this.listparams.fileStyleId = this.docTypeKey.id;
+        this.initList();
+      },
+      onRowClick(row) {
+        this.$router.push("/allfiles/check/" + row.id);
+      },
+      onPageChange(value) {
+        this.listparams.current = value;
+        this.initList();
+      },
+      onPageSizeChange(value) {
+        this.listparams.pageSize = value;
+        this.initList();
+      },
+      initList() {
+        console.log("个人能看到的文件展示列表");
+        console.log(this.listparams);
+        showUserLookFile(this.listparams)
+          .then(res => {
+            const showUserUpdata = res.data;
+            console.log(showUserUpdata);
+            if (res.data.code == 0) {
+              this.page = res.data.rdPage;
+              this.userLookFileList = showUserUpdata.data;
+            }
+          })
+          .catch(err => {
+          });
+      }
     }
-  }
-};
+  };
 </script>
 <style scoped>
-.page {
-  padding: 30px 10px;
-}
+  .page {
+    padding: 20px 10px;
+  }
 </style>
