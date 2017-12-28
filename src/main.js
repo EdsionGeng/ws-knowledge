fdeeba4a2e5aed7b84e1069587407d9c2c581443
李海涛 <15393243613@163.com>
2017-12-28 18:40:48 +0800

    修改：自动登录

--- src/main.js
+++ src/main.js
@@ -14,44 +14,36 @@ Vue.use(iView);
 Vue.config.productionTip = false
 let isAdmin = true;
 router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start();
   let params = {
     username: to.query.username,
     password: to.query.password
   }
   if (params.username != undefined) {
-    console.log(1111)
     sessionStorage.clear();
   }
-  console.log(sessionStorage)
-
   if (sessionStorage.getItem('isLogin')) {
-    if (sessionStorage.getItem('isAdmin') !== 'undefined') {
-      if (to.path == "/login") { //判断下一个是不是跳login
-        next('/') //跳转到一个不同的地址，当前导航被中断，然后进行一个新的民航
-      } else {
-        next();
-      }
+    if (to.path == "/login") {
+      next('/')
     } else {
-      if (to.path == "/login") {
-        next();
-      } else {
-        next('/login');
-      }
+      next();
     }
   } else {
     if (params.username != undefined && params.password != undefined) {
       login(params).then(res => {
         const data = res.data;
-        if (data.code === 200) {
-          const users = data.data.users;
-          const id = users.id;
-          const token = data.data.token;
+        if (data.code == 0) {
+          let username = data.data.username;
+          let id = data.data.id;
+          let userGroupId = data.data.UserGroupId;
           sessionStorage.setItem("isLogin", true);
-          sessionStorage.setItem("id", id);
-          sessionStorage.setItem("token", token);
+          sessionStorage.setItem("userGroupId", userGroupId);
+          sessionStorage.setItem("username", username);
+          sessionStorage.setItem("userId", id);
           next();
-        } else if (data.code === 500) {
+        } else if (data.code == 2) {
           next('/login');
+          // window.location.href = "http://report.wsloan.com:8888/wsdm/login";
         }
       })
     } else {
@@ -59,12 +51,16 @@ router.beforeEach((to, from, next) => {
         next();
       } else {
         next('/login');
+        // window.location.href = "http://report.wsloan.com:8888/wsdm/login";
       }
     }
   }
 });
 
 
+router.afterEach((to, from) => {
+  iView.LoadingBar.finish();
+});
 
 /* eslint-disable no-new */
 new Vue({
