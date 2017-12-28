6ff00cacd5adc88043888096c3e4e3e9215fa04a
李海涛 <15393243613@163.com>
2017-12-28 18:10:41 +0800

    新建：公共url提取

---
+++ src/utils/commonurl.js
@@ -0,0 +1,22 @@
+let baseURL="";
+if(process.env.NODE_ENV ==='development'){
+    baseURL = 'http://192.168.3.26:8011';
+}else{
+    if(process.env.type ==="test"){
+        baseURL='http://192.168.3.26:8011';
+    }else{
+        baseURL="http://report.wsloan.com:8888/knowledge/";
+        // console.log(baseURL);
+    }
+}
+export function getUploadUrl(){
+    let uploadUrl = baseURL+'/downLoadFile';
+    // let uploadUrl = baseurl+'/projectManage/uploadFile';
+    return uploadUrl;
+}
+
+export function getRequestUrl(){
+    let requestUrl = baseURL;
+    // let requestUrl = baseurl+'/projectManage';
+    return requestUrl;
+}
