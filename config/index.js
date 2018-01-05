155a10ebd9b742ac1d38a40e43817cc16b2f2c03
zw <18755489161@163.com>
2018-01-05 10:46:44 +0800

    更改：index.js+开发环境中的路径

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/ws-knowledge/',
     proxyTable: {
     },
     
